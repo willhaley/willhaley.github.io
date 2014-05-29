2a636d9ffb3da0ce59324d7c87db7985f1494184
Will <whaley@capstrat.com>
2014-05-28 22:21:33 -0400

    Adds some colors and fills in some blanks.

--- src/js/gameBoard.js
+++ src/js/gameBoard.js
@@ -7,17 +7,25 @@ var gameBoard = {
 
   drawBoard: function(svg, tiles, color){
 
-    _.forEach( tiles, function(tile){
-      var tileText = svg.select('g#'+tile.id + ' text');
-      var tilePath = svg.select('g#'+tile.id + ' path');
+    _.forEach( tiles, function(tile) {
+      var tileText = svg.select('g#' + tile.id + ' text');
+      var tilePath = svg.select('g#' + tile.id + ' path');
 
-          tileText.node.textContent = tile.value ;
+      tileText.node.textContent = tile.value;
+      tileText.attr({"font-size": "72px"});
 
-          if ( tile.value )
-            tilePath.attr({ fill: color[tile.value] });
-          else{
-            tilePath.attr({ fill: '#FFF' });
-          }
+      if (tile.value){
+
+        if ( tile.value > 10 ) {
+          tileText.attr({"font-size": "50px"});
+        }
+
+        tilePath.attr({fill: color[tile.value]});
+      }
+      else{
+        tilePath.attr({ fill: '#FFF' });
+      }
+
     });
 
   }
